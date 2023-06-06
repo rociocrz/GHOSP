56a321ba63d8c807c93856f1e4a2127c3938f2d3
Dani4262 <alfredodanielramos123456@gmail.com>
2023-06-05 19:38:36 -0600

    modificacino

---
+++ validar.php
@@ -0,0 +1,34 @@
+<?php
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+session_start();
+$_SESSION['usuario']=$usuario;
+
+$conexion=mysqli_connect("localhost","root","","rol");
+
+$consulta="SELECT*FROM usuarios where usuario='$usuario' and contraseña='$contraseña'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_fetch_array($resultado);
+
+if($filas['id_cargo']==1){ //administrador
+    header("location:principal.html");
+
+}else
+if($filas['id_cargo']==2){ //Cajero
+header("location:principal.html");
+
+}else
+if($filas['id_cargo']==3){ //supervisor
+header("location:principal.html");
+}
+else{
+    ?>
+    <?php
+    include("index.html");
+    ?>
+    <h1 class="bad">ERROR EN LA AUTENTIFICACION</h1>
+    <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
